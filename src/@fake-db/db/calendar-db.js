ecb63ab5856c0ea1f0f49bede4e3b3c08236d5a1
BrokeTheInternet <brandonotero14@gmail.com>
2019-06-27 22:40:54 -0400

    Fix

--- src/@fake-db/db/calendar-db.js
+++ src/@fake-db/db/calendar-db.js
@@ -5,105 +5,24 @@ const calendarDB = {
     events: [
         {
             id    : 0,
-            title : 'All Day Event very long title',
+            title :'CHOP Zoo Event',
             allDay: true,
-            start : new Date(2018, 3, 0),
-            end   : new Date(2018, 3, 1)
+            start : new Date(2019, 5, 26),
+            end   : new Date(2019, 5, 26)
         },
         {
             id    : 1,
-            title : 'Long Event',
-            allDay: false,
-            start : new Date(2018, 3, 7),
-            end   : new Date(2018, 3, 10)
+            title : 'Project Presentation + Last Day of Class',
+            allDay: true,
+            start : new Date(2019, 5, 29),
+            end   : new Date(2019, 5, 29)
         },
         {
             id    : 2,
-            title : 'DTS STARTS',
-            allDay: false,
-            start : new Date(2019, 2, 13, 0, 0, 0),
-            end   : new Date(2019, 2, 20, 0, 0, 0)
-        },
-        {
-            id    : 3,
-            title : 'DTS ENDS',
-            allDay: false,
-            start : new Date(2019, 10, 6, 0, 0, 0),
-            end   : new Date(2019, 10, 13, 0, 0, 0)
-        },
-        {
-            id    : 4,
-            title : 'Some Event',
-            allDay: false,
-            start : new Date(2018, 3, 9, 0, 0, 0),
-            end   : new Date(2018, 3, 9, 0, 0, 0)
-        },
-        {
-            id    : 5,
-            title : 'Conference',
-            allDay: false,
-            start : new Date(2018, 3, 11),
-            end   : new Date(2018, 3, 13),
-            desc  : 'Big conference for important people'
-        },
-        {
-            id    : 6,
-            title : 'Meeting',
-            allDay: false,
-            start : new Date(2018, 3, 12, 10, 30, 0, 0),
-            end   : new Date(2018, 3, 12, 12, 30, 0, 0),
-            desc  : 'Pre-meeting meeting, to prepare for the meeting'
-        },
-        {
-            id    : 7,
-            title : 'Lunch',
-            allDay: false,
-            start : new Date(2018, 3, 12, 12, 0, 0, 0),
-            end   : new Date(2018, 3, 12, 13, 0, 0, 0),
-            desc  : 'Power lunch'
-        },
-        {
-            id    : 8,
-            title : 'Meeting',
-            allDay: false,
-            start : new Date(2018, 3, 12, 14, 0, 0, 0),
-            end   : new Date(2018, 3, 12, 15, 0, 0, 0)
-        },
-        {
-            id    : 9,
-            title : 'Happy Hour',
-            allDay: false,
-            start : new Date(2018, 3, 12, 17, 0, 0, 0),
-            end   : new Date(2018, 3, 12, 17, 30, 0, 0),
-            desc  : 'Most important meal of the day'
-        },
-        {
-            id    : 10,
-            title : 'Dinner',
-            allDay: false,
-            start : new Date(2018, 3, 12, 20, 0, 0, 0),
-            end   : new Date(2018, 3, 12, 21, 0, 0, 0)
-        },
-        {
-            id    : 11,
-            title : 'Birthday Party',
-            allDay: false,
-            start : new Date(2018, 3, 13, 7, 0, 0),
-            end   : new Date(2018, 3, 13, 10, 30, 0)
-        },
-        {
-            id    : 12,
-            title : 'Late Night Event',
-            allDay: false,
-            start : new Date(2018, 3, 17, 19, 30, 0),
-            end   : new Date(2018, 3, 18, 2, 0, 0)
-        },
-        {
-            id    : 13,
-            title : 'Multi-day Event',
-            allDay: false,
-            start : new Date(2018, 3, 20, 19, 30, 0),
-            end   : new Date(2018, 3, 22, 2, 0, 0)
+            title : '4th of July',
+            allDay: true,
+            start : new Date(2019, 6, 4),
+            end   : new Date(2019, 6, 4)
         }
     ]
 };
