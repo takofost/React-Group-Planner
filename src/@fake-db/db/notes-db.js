1652b2db8e5c3fb760f9dbde0356e051863aea0d
Kenny Tran <trank7295@gmail.com>
2019-06-28 21:00:49 -0400

    more updates

--- src/@fake-db/db/notes-db.js
+++ src/@fake-db/db/notes-db.js
@@ -1,119 +1,137 @@
-import mock from './../mock';
+import moment from 'moment';
+import { isModuleSpecifier } from '@babel/types';
 import {FuseUtils} from '@fuse';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const axios = require('axios');
 
-const notesDB = {
-    notes : [
+//const notesDB = {
+    // notes : [
 
-        {
-            "id"         : "5739d1fb46da846f60d70b2c",
-            "title"      : "Todo",
-            "description": "",
-            "archive"    : false,
-            "image"      : "",
-            "time"       : "2019-06-29T10:00:00.000Z",
-            "reminder"   : "2019-04-25T10:10:00.000Z",
-            "checklist"  : [
-                {
-                    "id"     : "1",
-                    "checked": false,
-                    "text"   : "Present Final"
-                },
-                {
-                    "id"     : "2",
-                    "checked": true,
-                    "text"   : "Feed the dog"
-                }
-            ],
-            "labels"     : [
-                "5725a6806acf030f9341e925"
-            ]
-        },
+    //     {
+    //         "id"         : "5739d1fb46da846f60d70b2c",
+    //         "title"      : "Todo",
+    //         "description": "",
+    //         "archive"    : false,
+    //         "image"      : "",
+    //         "time"       : "2019-06-29T10:00:00.000Z",
+    //         "reminder"   : "2019-04-25T10:10:00.000Z",
+    //         "checklist"  : [
+    //             {
+    //                 "id"     : "1",
+    //                 "checked": false,
+    //                 "text"   : "Present Final"
+    //             },
+    //             {
+    //                 "id"     : "2",
+    //                 "checked": true,
+    //                 "text"   : "Feed the dog"
+    //             }
+    //         ],
+    //         "labels"     : [
+    //             "5725a6806acf030f9341e925"
+    //         ]
+    //     },
 
 
-    ],
-    labels: [
-        {
-            "id"    : "5725a6802d10e277a0f35739",
-            "name"  : "Family",
-            "handle": "family"
-        },
-        {
-            "id"    : "5725a6809fdd915739187ed5",
-            "name"  : "Work",
-            "handle": "work"
-        },
-        {
-            "id"    : "5725a68031fdbb1db2c1af47",
-            "name"  : "Todos",
-            "handle": "todos"
-        },
-        {
-            "id"    : "5725a680606588342058356d",
-            "name"  : "Prior",
-            "handle": "prior"
-        },
-        {
-            "id"    : "5725a6806acf030f9341e925",
-            "name"  : "Personal",
-            "handle": "personal"
-        },
-        {
-            "id"    : "5725a6806acf030f9341e932",
-            "name"  : "Friends",
-            "handle": "friends"
-        }
-    ]
-};
+    // ],
 
-mock.onGet('/api/notes-app/notes').reply((config) => {
-    return [200, notesDB.notes];
-});
 
-mock.onGet('/api/notes-app/labels').reply((config) => {
-    return [200, notesDB.labels];
-});
+    // labels: [
+    //     {
+    //         "id"    : "5725a6802d10e277a0f35739",
+    //         "name"  : "Family",
+    //         "handle": "family"
+    //     },
+    //     {
+    //         "id"    : "5725a6809fdd915739187ed5",
+    //         "name"  : "Work",
+    //         "handle": "work"
+    //     },
+    //     {
+    //         "id"    : "5725a68031fdbb1db2c1af47",
+    //         "name"  : "Todos",
+    //         "handle": "todos"
+    //     },
+    //     {
+    //         "id"    : "5725a680606588342058356d",
+    //         "name"  : "Prior",
+    //         "handle": "prior"
+    //     },
+    //     {
+    //         "id"    : "5725a6806acf030f9341e925",
+    //         "name"  : "Personal",
+    //         "handle": "personal"
+    //     },
+    //     {
+    //         "id"    : "5725a6806acf030f9341e932",
+    //         "name"  : "Friends",
+    //         "handle": "friends"
+    //     }
+    // ]
+//};
 
-mock.onPost('/api/notes-app/create-note').reply((request) => {
-    const data = JSON.parse(request.data);
-    const newNote =
-        {
-            ...data.note,
-            id: FuseUtils.generateGUID()
-        };
-    notesDB.notes = [
-        newNote,
-        ...notesDB.notes
-    ];
-    return [200, newNote];
-});
+// let notesDB = new Schema({
+//   id:{type: String},
+//   description: {type: String},
+//   image: {type: String},
+//   time: { type: String,
+//     default: moment.now()}
 
-mock.onPost('/api/notes-app/update-note').reply((request) => {
-    const data = JSON.parse(request.data);
+// })
 
-    notesDB.notes = notesDB.notes.map((note) => {
-        if ( data.note.id === note.id )
-        {
-            return data.note
-        }
-        return note
-    });
+// isModuleSpecifier.exports = mongoose.model('notesDB',notesDB)
 
-    return [200, data.note];
-});
+// axios.get('/api/notes-app/notes')
+// .then(response){
+//     return [200, notesDB.notes];
+// };
 
-mock.onPost('/api/notes-app/update-labels').reply((request) => {
-    const data = JSON.parse(request.data);
+// axios.get('/api/notes-app/labels').then(response){
+//     return [200, notesDB.labels];
+// };
 
-    notesDB.labels = data.labels;
+// axios.post('/api/notes-app/create-note').then(response){
+//     const data = JSON.parse(request.data);
+//     const newNote =
+//         {
+//             ...data.note,
+//             id: FuseUtils.generateGUID()
+//         };
+//     notesDB.notes = [
+//         newNote,
+//         ...notesDB.notes
+//     ];
+//     return [200, newNote];
+// };
 
-    return [200, notesDB.labels];
-});
+// axios.post('/api/notes-app/update-note').then(response){
+//     const data = JSON.parse(request.data);
 
-mock.onPost('/api/notes-app/remove-note').reply((request) => {
-    const data = JSON.parse(request.data);
+//     notesDB.notes = notesDB.notes.map((note) => {
+//         if ( data.note.id === note.id )
+//         {
+//             return data.note
+//         }
+//         return note
+//     });
 
-    notesDB.notes = notesDB.notes.filter((note) => data.noteId !== note.id);
+//     return [200, data.note];
+// });
 
-    return [200, notesDB.notes];
-});
+// axios.post('/api/notes-app/update-labels').then(response){
+//     const data = JSON.parse(request.data);
+
+//     notesDB.labels = data.labels;
+
+//     return [200, notesDB.labels];
+// });
+
+// axios.delete('/api/notes-app/remove-note').then(response){
+//     const data = JSON.parse(request.data);
+
+//     notesDB.notes = notesDB.notes.filter((note) => data.noteId !== note.id);
+
+//     return [200, notesDB.notes];
+// });
 
